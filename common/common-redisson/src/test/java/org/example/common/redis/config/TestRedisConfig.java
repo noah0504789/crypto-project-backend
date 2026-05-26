e01478869436f9efced7519307068ff80d163e4b
noah kim <noah0504@naver.com>
2026-05-27 00:01:06 +0900

    [feat] common-redisson: common-redis로부터 분리

---
+++ common/common-redisson/src/test/java/org/example/common/redis/config/TestRedisConfig.java
@@ -0,0 +1,40 @@
+package org.example.common.redis.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@TestConfiguration
+public class TestRedisConfig {
+
+    @Primary
+    @Bean("redisTemplate")
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        StringRedisSerializer serializer = new StringRedisSerializer();
+        template.setKeySerializer(serializer);
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(serializer);
+        template.setHashValueSerializer(serializer);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(Environment environment) {
+        String host = environment.getProperty("spring.data.redis.host", "localhost");
+        int port = environment.getProperty("spring.data.redis.port", Integer.class, 6379);
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(host, port);
+        factory.afterPropertiesSet();
+        return factory;
+    }
+}
