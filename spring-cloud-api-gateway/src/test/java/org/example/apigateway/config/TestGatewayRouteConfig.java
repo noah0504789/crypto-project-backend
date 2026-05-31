89d0ac19f15a79c05e84bc000ad61f7945bd529f
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2026-05-31 16:26:24 +0900

    [refactor] api-gateway 패키지명 정리 (#17)

---
+++ spring-cloud-api-gateway/src/test/java/org/example/apigateway/config/TestGatewayRouteConfig.java
@@ -0,0 +1,53 @@
+package org.example.apigateway.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class TestGatewayRouteConfig {
+
+    @Bean
+    public RouteLocator testOauth2ClientRoutes(RouteLocatorBuilder r, TestDownstreamServerConfig.TestDownstreamServers downstreamServers) {
+        return r.routes()
+                .route("oauth2-client-route", spec -> spec
+                        .path("/oauth2/**", "/login/oauth2/code/*", "/auth/**")
+                        .filters(f -> f
+                                .addRequestHeader("X-From", "gateway")
+                                .addResponseHeader("X-Gateway", "reactive")
+                        )
+                        .uri("http://localhost:" + downstreamServers.oauth2ClientPort())
+                )
+                .build();
+    }
+
+    @Bean
+    public RouteLocator testUserRoutes(RouteLocatorBuilder r, TestDownstreamServerConfig.TestDownstreamServers downstreamServers) {
+        return r.routes()
+                .route("user-route", s -> s
+                        .path("/user/**")
+                        .filters(f -> f
+                                .addRequestHeader("X-From", "gateway")
+                                .rewritePath("/user(?<seg>/.*)?$", "/api/v1/user${seg}")
+                                .addResponseHeader("X-Gateway", "reactive"))
+                        .uri("http://localhost:" + downstreamServers.userPort())
+                )
+                .build();
+    }
+
+    @Bean
+    public RouteLocator testChatRoutes(RouteLocatorBuilder r, TestDownstreamServerConfig.TestDownstreamServers downstreamServers) {
+        return r.routes()
+                .route("chat-route", s -> s
+                        .path("/chat/**")
+                        .filters(f -> f
+                                .addRequestHeader("X-From", "gateway")
+                                .rewritePath("/chat(?<seg>/.*)?$", "/api/v1/chat${seg}")
+                                .addResponseHeader("X-Gateway", "reactive")
+                        )
+                        .uri("http://localhost:" + downstreamServers.chatPort())
+                )
+                .build();
+    }
+}
