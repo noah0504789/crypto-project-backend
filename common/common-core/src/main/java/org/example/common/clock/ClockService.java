4e70088a0208fe2081d0ffee0887e8c9a8339e74
noah kim <noah0504@naver.com>
2026-06-23 11:32:53 +0900

    [feat] common-core: Clock-LocalDateTime용 now() 추가

--- common/common-core/src/main/java/org/example/common/clock/ClockService.java
+++ common/common-core/src/main/java/org/example/common/clock/ClockService.java
@@ -3,6 +3,9 @@ package org.example.common.clock;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+
+import static org.example.common.time.ServiceZoneUtils.ZONE_ID;
 
 @Service
 public class ClockService implements Clock {
@@ -15,5 +18,10 @@ public class ClockService implements Clock {
     @Override
     public Instant now() {
         return Instant.now();
+    }
+
+    @Override
+    public LocalDateTime nowLocalDateTime() {
+        return LocalDateTime.now(ZONE_ID);
     }
 }
