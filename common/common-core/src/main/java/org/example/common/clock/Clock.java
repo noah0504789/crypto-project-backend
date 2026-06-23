6f1d0542889e78651ab6dd21cf70d94b17c77030
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2026-06-24 00:00:57 +0900

    [feat] common-core: Clock-LocalDateTime용 now() 추가 (#63)

--- common/common-core/src/main/java/org/example/common/clock/Clock.java
+++ common/common-core/src/main/java/org/example/common/clock/Clock.java
@@ -1,8 +1,13 @@
 package org.example.common.clock;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 public interface Clock {
+
     long nowMs();
+
     Instant now();
+
+    LocalDateTime nowLocalDateTime();
 }
