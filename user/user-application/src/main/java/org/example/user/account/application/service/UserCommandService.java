c472ee423bd0c4b065a44f92e6072cfdb8a145f5
noah kim <noah0504@naver.com>
2026-06-30 00:35:15 +0900

    user:application

--- user/user-application/src/main/java/org/example/user/account/application/service/UserCommandService.java
+++ user/user-application/src/main/java/org/example/user/account/application/service/UserCommandService.java
@@ -1,9 +1,15 @@
 package org.example.user.account.application.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.user.account.application.port.in.UserCommandUseCase;
 import org.example.user.account.application.port.out.UserPersistencePort;
 import org.example.user.account.domain.exception.UserNotFoundException;
 import org.example.user.account.domain.model.User;
+import org.example.user.role.application.port.out.RolePersistencePort;
+import org.example.user.role.domain.exception.RoleNotFoundException;
+import org.example.user.role.domain.model.Role;
+import org.example.user.role.domain.model.RoleEnum;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,20 +17,49 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class UserCommandService {
+public class UserCommandService implements UserCommandUseCase {
 
     private final UserPersistencePort userRepository;
+    private final RolePersistencePort roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    @Transactional
-    public User save(User user) {
-        return userRepository.save(user);
-    }
-
+    @Override
     @Transactional
     public void updateProfile(UUID publicId, String nickname) {
         User user = userRepository.findByPublicId(publicId)
                 .orElseThrow(() -> new UserNotFoundException(publicId));
 
         user.updateNickname(nickname);
+    }
+
+    @Override
+    @Transactional
+    public User signUpOauth2(String sub, String email, String nickname) {
+        Role role = getDefaultRole();
+
+        User newUser = User.ofOAuth2(sub, email, nickname);
+        newUser.addRole(role);
+
+        return userRepository.save(newUser);
+    }
+
+    @Override
+    @Transactional
+    public User signUpLocal(String email, String nickname, String password) {
+        Role role = getDefaultRole();
+
+        String encodedPassword = passwordEncoder.encode(password);
+
+        User newUser = User.ofLocal(email, nickname, encodedPassword);
+        newUser.addRole(role);
+
+        return userRepository.save(newUser);
+    }
+
+    private Role getDefaultRole() {
+        RoleEnum defaultRole = User.getDefaultRole();
+
+        return roleRepository.findByName(defaultRole)
+                .orElseThrow(() -> new RoleNotFoundException(defaultRole));
     }
 }
