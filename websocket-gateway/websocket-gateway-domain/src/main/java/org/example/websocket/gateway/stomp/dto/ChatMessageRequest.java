f1bfbec3300768beedd392b11b9597b92310bd13
noah kim <noah0504@naver.com>
2026-06-30 15:27:50 +0900

    websocket:domain

--- websocket-gateway/websocket-gateway-domain/src/main/java/org/example/websocket/gateway/stomp/dto/ChatMessageRequest.java
+++ websocket-gateway/websocket-gateway-domain/src/main/java/org/example/websocket/gateway/stomp/dto/ChatMessageRequest.java
@@ -4,12 +4,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ChatMessageRequest(
+
         @NotBlank
         String clientMessageId,
+
         @NotBlank
         String roomId,
+
         @NotBlank
         String writerId,
+
         @NotBlank
         @Size(max = 1000)
         String content
