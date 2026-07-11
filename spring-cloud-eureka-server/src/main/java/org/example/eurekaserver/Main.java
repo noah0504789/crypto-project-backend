9e317635cf07cb0dc94dc03a5c43a085d5c87836
noah kim <noah0504@naver.com>
2026-07-12 00:20:40 +0900

    1

--- spring-cloud-eureka-server/src/main/java/org/example/eurekaserver/Main.java
+++ spring-cloud-eureka-server/src/main/java/org/example/eurekaserver/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.example")
 @EnableEurekaServer
 public class Main {
     public static void main(String[] args) {
